f2ab92e2309f5f28ca22ed4517250e98a6461cd7
Zechariah <zechariahtan144@gmail.com>
2022-02-27 22:23:16 +0800

    Finished challenge 2

---
+++ src/challenge_2.rs
@@ -0,0 +1,56 @@
+#[allow(dead_code)]
+fn unique<T: Ord>(vars: Vec<T>) -> Vec<T>
+where
+    T: PartialEq + Clone,
+{
+    let mut unique_vars = vec![];
+    for var in vars {
+        if !unique_vars.contains(&var) {
+            unique_vars.push(var.clone());
+        }
+    }
+
+    unique_vars.sort_by(|a, b| a.cmp(b));
+    unique_vars
+}
+
+#[test]
+fn empty_list() {
+    let input: Vec<i32> = vec![];
+    let expected_output = vec![];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn sorted_list() {
+    let input = vec![1, 4, 5];
+    let expected_output = vec![1, 4, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn unsorted_list() {
+    let input = vec![1, 5, 2];
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn unsorted_list_with_duplicates() {
+    let input = vec![1, 5, 2, 2, 1];
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn sorted_list_with_duplicates() {
+    let mut input = vec![1, 5, 2, 2, 1];
+    input.sort_by(|x, y| x.partial_cmp(y).unwrap());
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 mod challenge_1;
+mod challenge_2;
